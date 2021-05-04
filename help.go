9bd072dfbd051814dcaff16201d9bb61fee9e124
Mateus Macedo <63172367+MateusMaceedo@users.noreply.github.com>
2021-05-04 20:54:04 -0300

    Create help.go

---
+++ help.go
@@ -0,0 +1,18 @@
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+  "array"
+  "temp"
+  "drop"
+  "calculator"
+
+	"https://github.com/MateusMaceedo"
+)
+
+[ "https://github.com/MateusMaceedo" ] = base.import
